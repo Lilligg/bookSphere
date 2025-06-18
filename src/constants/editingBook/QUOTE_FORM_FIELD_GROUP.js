e81ca96fa9e755d04bed03b8c31dfa14f36c5389
Lilligg <lida.obydennova@yandex.ru>
2025-06-19 01:41:43 +0300

    исправление кода

--- src/constants/editingBook/QUOTE_FORM_FIELD_GROUP.js
+++ src/constants/editingBook/QUOTE_FORM_FIELD_GROUP.js
@@ -3,13 +3,13 @@ export const QUOTE_FORM_FIELD_GROUP = [
         title: "Цитаты",
         fields: [
             {
-                label: "Цитата",
-                name: "text",
+                label: "Автор цитаты",
+                name: "person",
                 type: "text",
             },
             {
-                label: "Автор цитаты",
-                name: "person",
+                label: "Цитата",
+                name: "text",
                 type: "text",
             },
         ]
